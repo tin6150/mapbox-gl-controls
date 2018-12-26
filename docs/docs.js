b21333ebd8f4f02939d71b11711d6bf09c728767
tin6150 <tin6150@gmail.com>
2018-12-25 22:56:13 -0800

    webpack: need to modify the .js file that govern what get packed into the bundle.  the html is just web page loading created example code

--- docs/docs.js
+++ docs/docs.js
@@ -4,10 +4,11 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import 'prismjs/themes/prism.css';
 import '../src/mapbox-gl-controls.css';
 import './docs.css';
-import exampleLocalization from './examples/localization';
+// commented out by Tin 
+//import exampleLocalization from './examples/localization';
 import exampleStyles from './examples/styles';
-import exampleZoomCompass from './examples/zoom-compass';
-import exampleRuler from './examples/ruler';
+//import exampleZoomCompass from './examples/zoom-compass';
+//import exampleRuler from './examples/ruler';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYnJhdmVjb3ciLCJhIjoiY2o1ODEwdWljMThwbTJ5bGk0a294ZmVybiJ9.kErON3w2kwEVxU5aNa-EqQ';
 
